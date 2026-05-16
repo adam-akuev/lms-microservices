26bfb9ed56375ccbbb97fe6e8c61791b9959e16b
Adam_Akuev <akuev2005@mail.ru>
2026-05-16 20:45:54 +0300

    refactor: update security configurations to use lms-security module

--- api-gateway/src/main/java/com/lms/gateway/config/GatewaySecurityConfig.java
+++ api-gateway/src/main/java/com/lms/gateway/config/GatewaySecurityConfig.java
@@ -1,2 +1,24 @@
-package com.lms.gateway.config;public class GatewaySecurityConfig {
+package com.lms.gateway.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
+import org.springframework.security.config.web.server.ServerHttpSecurity;
+import org.springframework.security.web.server.SecurityWebFilterChain;
+
+@Configuration
+@EnableWebFluxSecurity
+public class GatewaySecurityConfig {
+
+    @Bean
+    public SecurityWebFilterChain securityWebFilterChain(ServerHttpSecurity http) {
+        http
+                .csrf(ServerHttpSecurity.CsrfSpec::disable)
+                .authorizeExchange(exchange -> exchange
+                        .pathMatchers("/api/**").permitAll()
+                        .anyExchange().permitAll()
+                );
+
+        return http.build();
+    }
 }
