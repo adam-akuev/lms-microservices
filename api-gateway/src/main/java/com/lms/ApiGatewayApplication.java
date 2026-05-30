65fc4cdb89841178c3957ca6b94091432cf7d9fd
Adam_Akuev <akuev2005@mail.ru>
2026-05-30 18:15:43 +0300

    feat: add JWT authentication filter to API Gateway

--- api-gateway/src/main/java/com/lms/ApiGatewayApplication.java
+++ api-gateway/src/main/java/com/lms/ApiGatewayApplication.java
@@ -2,9 +2,14 @@ package com.lms;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.reactive.ReactiveSecurityAutoConfiguration;
+import org.springframework.boot.autoconfigure.security.reactive.ReactiveUserDetailsServiceAutoConfiguration;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {
+        ReactiveSecurityAutoConfiguration.class,
+        ReactiveUserDetailsServiceAutoConfiguration.class,
+})
 @EnableDiscoveryClient
 public class ApiGatewayApplication {
     public static void main(String[] args) {
